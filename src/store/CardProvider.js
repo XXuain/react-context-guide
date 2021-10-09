6407f1a263a96ae9d289b220d0d77ee769feb38f
yellowHuang <hlady1115@gmail.com>
2021-10-09 20:05:03 +0800

    add: handle existingItem amount

--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -9,9 +9,23 @@ const defaultCartState = {
 const cardReducer = (state, action) => {
   const actionTypeHandle = {
     ADD: () => {
-      const updateItems = state.items.concat(action.item);
       const updateTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
+      const existingIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      let updateItems;
+
+      if (state.items[existingIndex]) {
+        updateItems = [...state.items];
+        updateItems[existingIndex] = {
+          ...state.items[existingIndex],
+          amount: state.items[existingIndex].amount + action.item.amount,
+        };
+      } else {
+        updateItems = state.items.concat(action.item);
+      }
+
       return {
         items: updateItems,
         totalAmount: updateTotalAmount,
