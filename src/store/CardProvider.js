8d95c9765583598a5ef90816a573aad286b12d57
yellowHuang <hlady1115@gmail.com>
2021-10-09 18:24:17 +0800

    add: adding car reducer

--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -1,13 +1,46 @@
+import { useReducer } from "react";
 import CardContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cardReducer = (state, action) => {
+  const actionTypeHandle = {
+    ADD: () => {
+      const updateItems = state.items.concat(action.item);
+      const updateTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return {
+        items: updateItems,
+        totalAmount: updateTotalAmount,
+      };
+    },
+    REMOVE: () => {},
+  };
+  return actionTypeHandle[action.type]
+    ? actionTypeHandle[action.type]()
+    : defaultCartState;
+};
+
 const CardProvider = ({ children }) => {
-  const addItemToCardHandler = (item) => {};
-  const removeItemFormCardHandler = (id) => {};
+  const [cardState, dispatchCartAction] = useReducer(
+    cardReducer,
+    defaultCartState
+  );
+
+  const addItemToCardHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item });
+  };
+  const removeItemFormCardHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id });
+  };
 
   // this will be dynamic
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cardState.items,
+    totalAmount: cardState.totalAmount,
     addItem: addItemToCardHandler,
     removeItem: removeItemFormCardHandler,
   };
